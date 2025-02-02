45813967d22f483c7674e865a3dea4480c1677a7
Brendan Mitchell <brendan.mitchell@sisu.us>
2025-02-02 14:39:54 -0600

    Optimize for speed

--- src/bucket_sort/bucket_sort.cpp
+++ src/bucket_sort/bucket_sort.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 namespace {
-    std::function<int(double)> createLinearMapping(double inputMin, double inputMax, int outputSize) {
+    auto createLinearMapping(double inputMin, double inputMax, int outputSize) {
         const double linearMappingSlope = (outputSize) / (inputMax - inputMin);
         const double linearMappingOffset = -inputMin * linearMappingSlope;
         return [linearMappingSlope, linearMappingOffset, outputSize](double value) {
@@ -16,6 +16,16 @@ namespace {
             if (index >= outputSize) index = outputSize - 1;
             return index;
         };
+    }
+
+    std::pair<double, double> minMax(const std::vector<double>& data) {
+        double min = data.at(0);
+        double max = data.at(0);
+        for (double item : data) {
+            if (item < min) min = item;
+            if (item > max) max = item;
+        }
+        return std::pair<double, double> {min, max};
     }
 
     void insertionSort(std::vector<double>& data) {
@@ -52,14 +62,14 @@ void bucketSort(std::vector<double>& data, double bucketsPerItem = 1) {
     if (numBuckets < 1) {
         numBuckets = 1;
     }
-    std::vector<std::forward_list<double>> buckets(numBuckets);
+    std::vector<std::vector<double>> buckets(numBuckets);
 
     // Create a mapping of values to bucket indices
-    const auto [min, max] = std::ranges::minmax_element(data);
-    if (*min == *max) {
+    const auto [min, max] = minMax(data);
+    if (min == max) {
         return;
     }
-    const std::function<int(double)> linearMapping = createLinearMapping(*min, *max, numBuckets);
+    const std::function<int(double)> linearMapping = createLinearMapping(min, max, numBuckets);
 
     // Map the data to buckets
     for (double value : data) {
@@ -67,11 +77,11 @@ void bucketSort(std::vector<double>& data, double bucketsPerItem = 1) {
         if (index > buckets.size()) {
             std::cout << linearMapping(0) << " " << linearMapping(10) << std::endl;
         }
-        buckets.at(index).push_front(value);
+        buckets.at(index).push_back(value);
     }
 
     int index = 0;
-    for (const std::forward_list<double>& bucket : buckets) {
+    for (const std::vector<double>& bucket : buckets) {
         for (double value : bucket) {
             data.at(index++) = value;
         }
