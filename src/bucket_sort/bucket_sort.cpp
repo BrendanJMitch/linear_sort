8b40578b6ee692a8e4fb9389761c2ae4258e295b
Brendan Mitchell <brendan.mitchell@sisu.us>
2025-02-04 22:02:40 -0600

    Optimize further by removing vectors

--- src/bucket_sort/bucket_sort.cpp
+++ src/bucket_sort/bucket_sort.cpp
@@ -1,6 +1,8 @@
 #include "bucket_sort.hpp"
 
 #include <algorithm>
+#include <cmath>
+#include <cstring>
 #include <forward_list>
 #include <functional>
 #include <iostream>
@@ -42,50 +44,95 @@ namespace {
             data.at(j + 1) = temp;
         }
     }
+
+    class Bucket {
+
+      public:
+        Bucket(const int startingSize) :
+            size(startingSize), numItems(0), items(new double[startingSize]) { }
+
+        Bucket(const Bucket& other) : size(other.size), numItems(0), items(new double[other.size]) { }
+
+        ~Bucket() { delete[] items; }
+
+        void add(double item) {
+            if (numItems == size) resize();
+            items[numItems++] = item;
+        }
+
+        double* begin() { return items; }
+
+        double* end() { return items + numItems; }
+
+        const double* begin() const { return items; }
+
+        const double* end() const { return items + numItems; }
+
+      private:
+        int size;
+        int numItems;
+        double* items;
+
+        void resize() {
+            int newSize = size * 2;
+            double* newItems {new double[newSize]};
+            std::memcpy(newItems, items, numItems * sizeof(double));
+            delete[] items;
+            items = newItems;
+            size = newSize;
+        }
+    };
+
+    Bucket* getBuckets(int numBuckets, int bucketSize) {
+        Bucket* buckets = static_cast<Bucket*>(operator new[](numBuckets * sizeof(Bucket)));
+
+        for (int i = 0; i < numBuckets; ++i) {
+            new (&buckets[i]) Bucket(bucketSize);
+        }
+        return buckets;
+    }
 }
 
 /**
- * @brief Sort a vector of numeric data in O(n) time .
+ * @brief Sort a vector of numeric data using a bucket sort algorithm.
  *
  *
  * @param data A reference to a vector of unsorted numeric data
  * @param bucketsPerItem The number of buckets to use per item
  */
 void bucketSort(std::vector<double>& data, double bucketsPerItem = 1) {
-    if (data.size() == 0) {
-        return;
-    }
-    if (bucketsPerItem <= 0) {
-        throw std::invalid_argument("bucketsPerItem must be greater than 0");
-    }
+    if (data.size() == 0) return;
+
+    if (bucketsPerItem <= 0) throw std::invalid_argument("bucketsPerItem must be greater than 0");
+
     int numBuckets = static_cast<int>(data.size() * bucketsPerItem);
-    if (numBuckets < 1) {
-        numBuckets = 1;
-    }
-    std::vector<std::vector<double>> buckets(numBuckets);
+    if (numBuckets < 1) numBuckets = 1;
+
+    int bucketSize = std::ceil(1.5 / bucketsPerItem);
+    if (bucketSize < 1) bucketSize = 1;
+
+    Bucket* buckets = getBuckets(numBuckets, bucketSize);
 
     // Create a mapping of values to bucket indices
     const auto [min, max] = minMax(data);
-    if (min == max) {
-        return;
-    }
+    if (min == max) return;
+
     const std::function<int(double)> linearMapping = createLinearMapping(min, max, numBuckets);
 
     // Map the data to buckets
     for (double value : data) {
-        unsigned int index = linearMapping(value);
-        if (index > buckets.size()) {
-            std::cout << linearMapping(0) << " " << linearMapping(10) << std::endl;
-        }
-        buckets.at(index).push_back(value);
+        buckets[linearMapping(value)].add(value);
     }
 
     int index = 0;
-    for (const std::vector<double>& bucket : buckets) {
-        for (double value : bucket) {
+    for (int i = 0; i < numBuckets; i++) {
+        for (double value : buckets[i]) {
             data.at(index++) = value;
         }
+        buckets[i].~Bucket();
     }
+    operator delete[](buckets);
+
     insertionSort(data);
     return;
 }
